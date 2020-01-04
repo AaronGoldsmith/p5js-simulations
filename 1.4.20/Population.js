190e05061cfebfe9362d8e596ac6049dea7fed06
Aaron Goldsmith <agoldsmith@alumni.scu.edu>
2020-01-04 03:39:10 -0800

    actor driver

--- 1.4.20/Population.js
+++ 1.4.20/Population.js
@@ -26,22 +26,10 @@ function Population(){
        let close = current.checkCollisions(this.hotzone);
        if(close){
          // steer away
-         
-         let angle = random(-1/3*PI,-2/3*PI);
-         let angle2 = random(1/3*PI, 2/3*PI);
-         let mid = random() < 0.5 ? angle : angle2;
-         let pos = current.pos.copy();
-         let vel = current.vel.copy();
-         let _vel = vel.rotate(random(-1,1)*90).mult(2);
-         
-         // let desired = p5.Vector.sub(pos.add(_vel),this.actors[j].pos);
-         // desired.setMag(2);
-         // let diff = p5.Vector.sub(desired, this.actors[j].vel);
-         // diff.limit(0.1);
-         // this.actors[j].applyForce(diff);
-         // this.actors[j].vel = _vel;
-         // this.actors[j].pos.add(_vel);
-         // this.actors[j].crashing = true;
+         let pos = this.hotzone[0].copy();
+          current.setRed();
+         let _pos = createVector(random(width),random(height))
+         current.seek(_pos);
        }
       else{ this.actors[j].crashing = false;}
         // this.actors[j].discover(this.hotzone, this.popsize)
